e2924a3976a3895808b3185da4aa930f5384b6a3
Steven Leiva <leiva.steven@gmail.com>
2020-01-03 16:18:41 -0600

    Use the "Has Typeclass" approach to de-couple code.
    Before this change, our `program` was hard-coded to the `Env` datatype.
    With this change:
    
    1. Our `program` has no knowledge of the `Env` datatype.
    2. The type signature of `program` is much more informative; it tells us
    exactly what values in `Env` `program` utilizes.
    3. Finally, although it is not obvious in this example, the "has
    typeclass approach" makes it easier to test our functions because we
    only need to provide the exact environment that our functions need, as
    opposed to the entire environment.

--- HelloReaderTPattern.hs
+++ HelloReaderTPattern.hs
@@ -1,20 +1,26 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-11.6 script
 
-{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE InstanceSigs #-}
 
 import Control.Monad.Reader (ReaderT, MonadReader)
-import Control.Monad.Reader (runReaderT, reader)
+import Control.Monad.Reader (runReaderT, ask)
 
 import Control.Monad.IO.Class (MonadIO)
 import Control.Monad.IO.Class (liftIO)
 
--- 1) Core datatype
 data Env = Env { envName :: String }
+
+class HasName env where
+    getName :: env -> String
+
+instance HasName Env where
+    getName :: Env -> String
+    getName env = envName env
 
 main :: IO ()
 main = do
-    env <- initializeEnv -- 2) Initialize runtime configuration in main
+    env <- initializeEnv
     runReaderT program env
 
 initializeEnv :: IO Env
@@ -22,10 +28,12 @@ initializeEnv = do
     name <- prompt "Enter a name" >> getLine
     return (Env { envName = name })
 
-program :: (MonadIO m, MonadReader Env m) => m () -- 3) Write functions in terms of mtl-style typeclasses
+-- Our program has no knowledge of the Env data type. All it requires is that
+-- the environment has an instance of HasName.
+program :: (HasName env, MonadIO m, MonadReader env m) => m ()
 program = do
-  name <- reader envName
-  liftIO $ putStrLn ("Hello" <> " " <> name)
+    name <- getName <$> ask
+    liftIO $ putStrLn ("Hello" <> " " <> name)
 
 prompt :: String -> IO ()
 prompt str = putStr (str <> ": ")

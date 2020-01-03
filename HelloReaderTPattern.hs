3aa42d8a5069a8030b72229b26b59ea903b24142
Steven Leiva <leiva.steven@gmail.com>
2020-01-03 16:01:14 -0600

    Small example using ReaderT Design Pattern guidelines.
    Notice:
    
    1. We have a core data type that contains runtime configuration
    2. We initialize the core data type in our main function
    3. We write our program / functions using mtl-style constraints.

--- HelloReaderTPattern.hs
+++ HelloReaderTPattern.hs
@@ -1,24 +1,31 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-11.6 script
 
-{-
+{-# LANGUAGE FlexibleContexts #-}
 
-In order to implement the "ReaderT Design Pattern", we want to adhere to the following guidelines:
+import Control.Monad.Reader (ReaderT, MonadReader)
+import Control.Monad.Reader (runReaderT, reader)
 
-1) Define a core datatype (often called "Env")
+import Control.Monad.IO.Class (MonadIO)
+import Control.Monad.IO.Class (liftIO)
 
-2) Core datatype will contain (a) runtime configuration and (b) any functions
-that we may like to be able to mock and (c) mutable reference (IORef, TVar,
-etc.) for mutable state.
-
-3) Application code will live in a "ReaderT Env IO" monad. We can define a type
-alias or a newtype wrapper. We can use other monadic stacks for pure cord.
-
-4) Our code should be written against mtl-like interface such as MonadReader and
-MonadIO. This will allow us to recover some of the purity we "lost" given that
-the base monad in our transformer stack is IO.
-
- -}
+-- 1) Core datatype
+data Env = Env { envName :: String }
 
 main :: IO ()
-main = putStrLn "hello"
+main = do
+    env <- initializeEnv -- 2) Initialize runtime configuration in main
+    runReaderT program env
+
+initializeEnv :: IO Env
+initializeEnv = do
+    name <- prompt "Enter a name" >> getLine
+    return (Env { envName = name })
+
+program :: (MonadIO m, MonadReader Env m) => m () -- 3) Write functions in terms of mtl-style typeclasses
+program = do
+  name <- reader envName
+  liftIO $ putStrLn ("Hello" <> " " <> name)
+
+prompt :: String -> IO ()
+prompt str = putStr (str <> ": ")

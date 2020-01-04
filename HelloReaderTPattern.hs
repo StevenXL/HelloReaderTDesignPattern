bd5e3ca8eb784f7f7cab3444d5d45ffa138752ba
Steven Leiva <leiva.steven@gmail.com>
2020-01-04 09:50:31 -0600

    Use the "Has Typeclass" approach to make testing easier.
    In the previous commit, we saw how the "Has Typeclass" approach allowed
    us to de-couple our computations from our `Env` core datatype. Instead
    of having computations that know everything about their environment , we
    have computations that care about specific values in their environment.
    
    Now that our code is de-coupled, we can test our computations AND only
    provide the environment that that computation cares about. Notice that
    in the test for `computeGreeting`, we provided only a name as the
    environment, since that is all that `computeGreeting` has knowledge of /
    cares about.

--- HelloReaderTPattern.hs
+++ HelloReaderTPattern.hs
@@ -1,7 +1,11 @@
 #!/usr/bin/env stack
--- stack --resolver lts-11.6 script
+-- stack --resolver lts-14.19 script
 
 {-# LANGUAGE InstanceSigs #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE FlexibleInstances #-}
+
+import Test.Hspec (hspec, describe, it, shouldBe) -- 1) Import testing framework
 
 import Control.Monad.Reader (ReaderT, MonadReader)
 import Control.Monad.Reader (runReaderT, ask)
@@ -18,22 +22,32 @@ instance HasName Env where
     getName :: Env -> String
     getName env = envName env
 
-main :: IO ()
-main = do
-    env <- initializeEnv
-    runReaderT program env
+instance HasName String where
+    getName :: String -> String
+    getName str = str
+
+main :: IO () -- 2) Write a spec for the computeGreeting computation
+main = hspec $ do
+    describe "computeGreeting" $ do
+        it "computes the correct greeting" $ do
+            let ada = "Ada Lovelace" -- 3) Thanks to the "Has- typeclass approach", we can execute computeGreeting with only the environment it cares about
+            res <- runReaderT computeGreeting ada
+            res `shouldBe` "Hello, Ada Lovelace"
 
 initializeEnv :: IO Env
 initializeEnv = do
     name <- prompt "Enter a name" >> getLine
     return (Env { envName = name })
 
--- Our program has no knowledge of the Env data type. All it requires is that
--- the environment has an instance of HasName.
 program :: (HasName env, MonadIO m, MonadReader env m) => m ()
 program = do
+    greeting <- computeGreeting
+    liftIO $ putStrLn greeting
+
+computeGreeting :: (HasName env, MonadReader env m) => m String
+computeGreeting = do
     name <- getName <$> ask
-    liftIO $ putStrLn ("Hello" <> " " <> name)
+    pure $ ("Hello, " <> name)
 
 prompt :: String -> IO ()
 prompt str = putStr (str <> ": ")
